8511d5fde8df6823c67df9df281fc3b229f790d5
Zach McWhirter <zmac7811@gmail.com>
2020-06-06 23:40:02 -0500

    worked on the js to push the objects to my array

---
+++ src/scripts/journal.js
@@ -0,0 +1,17 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const journalEntryObject = {
+        dateOfEntry: "",
+        conceptsCovered: "",
+        journalEntry: "",
+        moodForTheDay: ""
+    }
+
+const arrayOfJournalEntryObjects =[]
+for (let journalEntry of arrayOfJournalEntryObjects) {
+    arrayOfJournalEntryObjects.push(journalEntry)
+    console.log(arrayOfJournalEntryObjects)
+    
+}
