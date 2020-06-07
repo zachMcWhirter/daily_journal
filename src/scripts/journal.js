bf89016240b4736995942393ae0fdd427994cc9f
Zach McWhirter <zmac7811@gmail.com>
2020-06-07 17:29:22 -0500

    created the journal entry  objects and pushed them to an array

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,18 +1,39 @@
-/*
-    Define the keys and value for a JavaScript object that
-    represents a journal entry about what you learned today
-*/
-const journalEntryObject = {
-        dateOfEntry: "",
-        conceptsCovered: "",
-        journalEntry: "",
-        moodForTheDay: ""
-    }
 
-const arrayOfJournalEntryObjects =[]
-for (const journalEntry of arrayOfJournalEntryObjects) {
-    arrayOfJournalEntryObjects.push(journalEntry)
+    // Define the keys and value for a JavaScript object that represents a journal entry about what you learned today
 
-    console.log(arrayOfJournalEntryObjects)
-    console.log(journalEntry) 
-}
+
+const arrayOfJournalEntryObjects =[];
+
+arrayOfJournalEntryObjects.push({
+    dateOfEntry: "06/01/20",
+    conceptsCovered: "design, flexbox, components, objects, properties",
+    journalEntry: "Martin's Aquarium Day 1",
+    moodForTheDay: "Pretty Good"
+});
+arrayOfJournalEntryObjects.push({
+    dateOfEntry: "06/02/20",
+    conceptsCovered: "functions, string templates, iteration",
+    journalEntry: "Martin's Aquarium Day 2",
+    moodForTheDay: "Fantastic!"
+});
+arrayOfJournalEntryObjects.push({
+    dateOfEntry: "06/03/20",
+    conceptsCovered: "functions, filtering, modulo, debugger",
+    journalEntry: "Martin's Aquarium Day 3",
+    moodForTheDay: "Hnaging In There"
+});
+arrayOfJournalEntryObjects.push({
+    dateOfEntry: "06/04/20",
+    conceptsCovered: "JSON, json-server, fetch, functions",
+    journalEntry: "Martin's Aquarium Day 4",
+    moodForTheDay: "Got Some Work To Do"
+});
+arrayOfJournalEntryObjects.push({
+    dateOfEntry: "06/05/20",
+    conceptsCovered: "browser events, functions, filtering, DOM",
+    journalEntry: "Martin's Aquarium Day 5",
+    moodForTheDay: "Really Good"
+});
+
+// console.log(arrayOfJournalEntryObjects)
+// should log an array of objects to the console
