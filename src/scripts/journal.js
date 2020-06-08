86c52cd7603685f1dd8357af9c67a361cea966bb
Zach McWhirter <zmac7811@gmail.com>
2020-06-07 22:59:32 -0500

    added styling to form

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,39 +1,88 @@
+//  Daily Journal - Part 2:
+//         Define the keys and value for a JavaScript object that represents a journal entry about what you learned today
 
-    // Define the keys and value for a JavaScript object that represents a journal entry about what you learned today
 
+// const arrayOfJournalEntryObjects =[];
 
-const arrayOfJournalEntryObjects =[];
-
-arrayOfJournalEntryObjects.push({
-    dateOfEntry: "06/01/20",
-    conceptsCovered: "design, flexbox, components, objects, properties",
-    journalEntry: "Martin's Aquarium Day 1",
-    moodForTheDay: "Pretty Good"
-});
-arrayOfJournalEntryObjects.push({
-    dateOfEntry: "06/02/20",
-    conceptsCovered: "functions, string templates, iteration",
-    journalEntry: "Martin's Aquarium Day 2",
-    moodForTheDay: "Fantastic!"
-});
-arrayOfJournalEntryObjects.push({
-    dateOfEntry: "06/03/20",
-    conceptsCovered: "functions, filtering, modulo, debugger",
-    journalEntry: "Martin's Aquarium Day 3",
-    moodForTheDay: "Hnaging In There"
-});
-arrayOfJournalEntryObjects.push({
-    dateOfEntry: "06/04/20",
-    conceptsCovered: "JSON, json-server, fetch, functions",
-    journalEntry: "Martin's Aquarium Day 4",
-    moodForTheDay: "Got Some Work To Do"
-});
-arrayOfJournalEntryObjects.push({
-    dateOfEntry: "06/05/20",
-    conceptsCovered: "browser events, functions, filtering, DOM",
-    journalEntry: "Martin's Aquarium Day 5",
-    moodForTheDay: "Really Good"
-});
+// arrayOfJournalEntryObjects.push({
+//     dateOfEntry: "06/01/20",
+//     conceptsCovered: "design, flexbox, components, objects, properties",
+//     journalEntry: "Martin's Aquarium Day 1",
+//     moodForTheDay: "Pretty Good"
+// });
+// arrayOfJournalEntryObjects.push({
+//     dateOfEntry: "06/02/20",
+//     conceptsCovered: "functions, string templates, iteration",
+//     journalEntry: "Martin's Aquarium Day 2",
+//     moodForTheDay: "Fantastic!"
+// });
+// arrayOfJournalEntryObjects.push({
+//     dateOfEntry: "06/03/20",
+//     conceptsCovered: "functions, filtering, modulo, debugger",
+//     journalEntry: "Martin's Aquarium Day 3",
+//     moodForTheDay: "Hnaging In There"
+// });
+// arrayOfJournalEntryObjects.push({
+//     dateOfEntry: "06/04/20",
+//     conceptsCovered: "JSON, json-server, fetch, functions",
+//     journalEntry: "Martin's Aquarium Day 4",
+//     moodForTheDay: "Got Some Work To Do"
+// });
+// arrayOfJournalEntryObjects.push({
+//     dateOfEntry: "06/05/20",
+//     conceptsCovered: "browser events, functions, filtering, DOM",
+//     journalEntry: "Martin's Aquarium Day 5",
+//     moodForTheDay: "Really Good"
+// });
 
 // console.log(arrayOfJournalEntryObjects)
 // should log an array of objects to the console
+
+******************************
+Daily Journal - Part 2:
+
+const arrayOfJournalEntryObjects =[
+    {
+        dateOfEntry: "06/01/20",
+        conceptsCovered: "design, flexbox, components, objects, properties",
+        journalEntry: "Martin's Aquarium Day 1",
+        moodForTheDay: "Pretty Good"
+    },
+    {
+        dateOfEntry: "06/02/20",
+        conceptsCovered: "functions, string templates, iteration",
+        journalEntry: "Martin's Aquarium Day 2",
+        moodForTheDay: "Fantastic!"
+    },
+    {
+        dateOfEntry: "06/03/20",
+        conceptsCovered: "functions, filtering, modulo, debugger",
+        journalEntry: "Martin's Aquarium Day 3",
+        moodForTheDay: "Hnaging In There"
+    },
+    {
+        dateOfEntry: "06/04/20",
+        conceptsCovered: "JSON, json-server, fetch, functions",
+        journalEntry: "Martin's Aquarium Day 4",
+        moodForTheDay: "Got Some Work To Do"
+    },
+    {
+        dateOfEntry: "06/05/20",
+        conceptsCovered: "browser events, functions, filtering, DOM",
+        journalEntry: "Martin's Aquarium Day 5",
+        moodForTheDay: "Really Good"
+    }
+]
+
+/*
+    Purpose: To create, and return, a string template that
+    represents a single journal entry object as HTML
+
+    Arguments: journalEntry (object)
+*/
+const makeJournalEntryComponent = (journalEntry) => {
+    // Create your own HTML structure for a journal entry
+    return `
+
+    `
+}
