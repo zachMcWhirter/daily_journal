5950478ee8fd35f20d398a9d46408ec60bb4cae5
Zach McWhirter <zmac7811@gmail.com>
2020-06-13 18:16:19 -0500

    daily-journal part 3, step 1

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -45,39 +45,38 @@ console.log("testing:journalEntries", journalEntries);
 //  *Open journal.js and place your journal entry objects inside the journal entries array. Note that you no longer need the variable names for each object. They are now simply elements in the journalEntries array.
 
 
-
-// const journalEntries = [
-//     {
-//         dateOfEntry: "06/01/20",
-//         conceptsCovered: "design, flexbox, components, objects, properties",
-//         journalEntry: "Martin's Aquarium Day 1",
-//         moodForTheDay: "Pretty Good"
-//     },
-//     {
-//         dateOfEntry: "06/02/20",
-//         conceptsCovered: "functions, string templates, iteration",
-//         journalEntry: "Martin's Aquarium Day 2",
-//         moodForTheDay: "Fantastic!"
-//     },
-//     {
-//         dateOfEntry: "06/03/20",
-//         conceptsCovered: "functions, filtering, modulo, debugger",
-//         journalEntry: "Martin's Aquarium Day 3",
-//         moodForTheDay: "Hnaging In There"
-//     },
-//     {
-//         dateOfEntry: "06/04/20",
-//         conceptsCovered: "JSON, json-server, fetch, functions",
-//         journalEntry: "Martin's Aquarium Day 4",
-//         moodForTheDay: "Got Some Work To Do"
-//     },
-//     {
-//         dateOfEntry: "06/05/20",
-//         conceptsCovered: "browser events, functions, filtering, DOM",
-//         journalEntry: "Martin's Aquarium Day 5",
-//         moodForTheDay: "Really Good"
-//     }
-// ]
+const journalEntries = [
+    {
+        dateOfEntry: "06/01/20",
+        conceptsCovered: "design, flexbox, components, objects, properties",
+        journalEntry: "Martin's Aquarium Day 1",
+        moodForTheDay: "Pretty Good"
+    },
+    {
+        dateOfEntry: "06/02/20",
+        conceptsCovered: "functions, string templates, iteration",
+        journalEntry: "Martin's Aquarium Day 2",
+        moodForTheDay: "Fantastic!"
+    },
+    {
+        dateOfEntry: "06/03/20",
+        conceptsCovered: "functions, filtering, modulo, debugger",
+        journalEntry: "Martin's Aquarium Day 3",
+        moodForTheDay: "Hnaging In There"
+    },
+    {
+        dateOfEntry: "06/04/20",
+        conceptsCovered: "JSON, json-server, fetch, functions",
+        journalEntry: "Martin's Aquarium Day 4",
+        moodForTheDay: "Got Some Work To Do"
+    },
+    {
+        dateOfEntry: "06/05/20",
+        conceptsCovered: "browser events, functions, filtering, DOM",
+        journalEntry: "Martin's Aquarium Day 5",
+        moodForTheDay: "Really Good"
+    }
+]
 
 //   *The last step in this chapter is to take your raw data structures, and create HTML representations of them so they can be added to the DOM.
 
