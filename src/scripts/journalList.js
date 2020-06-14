7923d29184c791c1953d9b88c8cb9a6693d14137
Zach McWhirter <zmac7811@gmail.com>
2020-06-13 21:13:12 -0500

    moved the journalEntryList function into the journalList.js module

---
+++ src/scripts/journalList.js
@@ -0,0 +1,29 @@
+// **************************
+// Daily Journal - Part 3 Instructions: STEP 3
+
+// **Render Journal Entries to the DOM**
+
+// Create a new element in your index.html file that will be the container for all of your journal entries. Place it beneath the form component.
+
+// src/index.html
+// ________________________________
+// <article class="entryLog">
+
+// </article>
+// _________________________________
+
+
+// *Now write a function whose reponsibility is to iterate your array of journal entries and add them to the DOM.
+
+// src/scripts/journal.js
+
+const journalEntryList = (journalEntryData) => {
+    for(const journalEntry of journalEntryData) {
+        const journalHTML = journalEntryConverter(journalEntry)
+        const journalEntryArticleElement = document.querySelector(".entrylog")
+        journalEntryArticleElement.innerHTML += journalHTML
+    }
+}
+
+//  Function will be invoked on main.js
+// journalEntryList(journalEntryData);
