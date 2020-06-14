36a2a941ea1d97f1a761212ee23043818f82b82b
Zach McWhirter <zmac7811@gmail.com>
2020-06-13 22:04:17 -0500

    added some details and styling to the entryLog article

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -8,10 +8,10 @@
 journalEntryConverter = (journalEntryObject) => {
     const journalEntryHTMLRepresentation = `<section class="journalEntryCard">
 
-    <div class="singleEntryDate">${journalEntryObject.dateOfEntry}</div> <br>
-    <div>${journalEntryObject.conceptsCovered}</div> <br>
-    <div>${journalEntryObject.journalEntry}</div> <br>
-    <div>${journalEntryObject.moodForTheDay}</div> <br>
+    <div class="singleEntryDate"><font color="white"; font-weight:bold;>Date:</font> ${journalEntryObject.dateOfEntry}</div> <br>
+    <div><font color="white"; font-weight:bold;>Concepts:</font> ${journalEntryObject.conceptsCovered}</div> <br>
+    <div><font color="white"; font-weight:bold;>Journal Entry:</font> ${journalEntryObject.journalEntry}</div> <br>
+    <div><font color="white"; font-weight:bold;>Mood:</font> ${journalEntryObject.moodForTheDay}</div> <br>
 </section>`
 
 return journalEntryHTMLRepresentation
