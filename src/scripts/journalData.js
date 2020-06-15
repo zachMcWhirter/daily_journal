fb65e2ec5d8bf984ab733fe280ae00a80da629ba
Zach McWhirter <zmac7811@gmail.com>
2020-06-14 20:34:19 -0500

    added function calls to main.js and added fetch call to journalData.js

--- src/scripts/journalData.js
+++ src/scripts/journalData.js
@@ -1,15 +1,19 @@
-// *******************************
-// Daily Journal - Part 3 Instructions:
-
-// *STEP 1*
-
-//  *Open journal.js and place your journal entry objects inside the journal entries array. Note that you no longer need the variable names for each object. They are now simply elements in the journalEntries array.
-
-const journalEntryData = []
-
 // ******************************
 // Daily Journal - Part 4 Instructions:
 
-// *Your next step is to remove the array of entries that you have in journal.js, and transplant it to api/entries.json as a JSON object.
+// *STEP 2* the fetch call
 
-// *When working with Official JSON Objects, all keys should be strings so put double quotes around them.
+let journalEntryData = []
+
+const getJournalEntryData = () => {
+    return fetch("http://localhost:3000/entries").then(
+            (httpResponse) => {
+                return httpResponse.json()
+        }
+    )
+        .then(
+            (arrayOfJournalEntries) => {
+                journalEntryData = arrayOfJournalEntries
+            }
+        )
+}
